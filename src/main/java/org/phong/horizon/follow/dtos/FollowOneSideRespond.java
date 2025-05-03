1d33077c0f5982135e034225d464881fccc06c4b
Phong <sanhok2104@gmail.com>
2025-05-03 21:26:49 +0700

    adjust follow section to use new dto UserIntroduction

--- src/main/java/org/phong/horizon/follow/dtos/FollowOneSideRespond.java
+++ src/main/java/org/phong/horizon/follow/dtos/FollowOneSideRespond.java
@@ -1,5 +1,6 @@
 package org.phong.horizon.follow.dtos;
 
+import org.phong.horizon.user.dtos.UserIntroduction;
 import org.phong.horizon.user.dtos.UserSummaryRespond;
 
 import java.io.Serializable;
@@ -8,5 +9,5 @@ import java.time.Instant;
 /**
  * DTO for {@link org.phong.horizon.follow.infrastructure.persistence.entities.Follow}
  */
-public record FollowOneSideRespond(UserSummaryRespond user, Instant createdAt) implements Serializable {
+public record FollowOneSideRespond(UserIntroduction user, Instant createdAt) implements Serializable {
 }
