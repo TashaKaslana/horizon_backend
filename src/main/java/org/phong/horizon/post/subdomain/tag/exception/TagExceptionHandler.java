ef0690164ea2717a9a9880b44ed56af573d645a2
Phong <sanhok2104@gmail.com>
2025-05-24 22:19:39 +0700

    refactor: use a dedicated exception handler in domains

--- src/main/java/org/phong/horizon/post/subdomain/tag/exception/TagExceptionHandler.java
+++ src/main/java/org/phong/horizon/post/subdomain/tag/exception/TagExceptionHandler.java
@@ -5,7 +5,6 @@ import org.phong.horizon.core.responses.RestApiResponse;
 import org.phong.horizon.core.utils.HttpRequestUtils;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
