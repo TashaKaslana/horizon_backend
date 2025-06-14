13200631c57c3d03760a836bba997bc5362b48f5
Phong <sanhok2104@gmail.com>
2025-06-14 21:38:45 +0700

    feat: integrate Ably event publishing in NotificationListener for notification creation

--- src/main/java/org/phong/horizon/notification/listeners/NotificationListener.java
+++ src/main/java/org/phong/horizon/notification/listeners/NotificationListener.java
@@ -1,16 +1,20 @@
 package org.phong.horizon.notification.listeners;
 
 import lombok.AllArgsConstructor;
+import org.phong.horizon.ably.event.AblyPublishableEvent;
 import org.phong.horizon.core.services.AuthService;
+import org.phong.horizon.core.utils.ObjectConversion;
 import org.phong.horizon.notification.events.CreateNotificationEvent;
 import org.phong.horizon.notification.events.NotificationPublishableEvent;
 import org.phong.horizon.notification.services.NotificationService;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+import java.util.Map;
 import java.util.UUID;
 
 @Component
@@ -18,6 +22,7 @@ import java.util.UUID;
 public class NotificationListener {
     private final NotificationService notificationService;
     private final AuthService authService;
+    private final ApplicationEventPublisher eventPublisher;
 
     @EventListener
     @Async
@@ -32,5 +37,22 @@ public class NotificationListener {
         UUID senderUserId = authService.getUserIdFromContext();
 
         notificationService.createEventNotification(senderUserId ,event.getNotificationRequest());
+
+        eventPublisher.publishEvent(new AblyPublishableEvent() {
+            @Override
+            public String getChannelName() {
+                return "notifications." + event.getNotificationRequest().getRecipientUserId();
+            }
+
+            @Override
+            public String getEventName() {
+                return "notification.created";
+            }
+
+            @Override
+            public Map<String, Object> getPayload() {
+                return ObjectConversion.convertObjectToFilteredMap(event.getNotificationRequest());
+            }
+        });
     }
 }
