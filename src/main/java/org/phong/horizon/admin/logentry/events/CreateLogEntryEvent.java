7a25fac145e9db62c6d64be03eb8d9f08f8d6a9a
Phong <sanhok2104@gmail.com>
2025-06-15 18:35:36 +0700

    feat(log-entry): implement event publishing for log entry creation

--- src/main/java/org/phong/horizon/admin/logentry/events/CreateLogEntryEvent.java
+++ src/main/java/org/phong/horizon/admin/logentry/events/CreateLogEntryEvent.java
@@ -1,28 +1,16 @@
 package org.phong.horizon.admin.logentry.events;
 
 import lombok.Getter;
-import org.phong.horizon.ably.event.AblyPublishableEvent;
 import org.phong.horizon.admin.logentry.dtos.CreateLogEntryRequest;
-import org.phong.horizon.admin.logentry.utils.LogEntryChannelNames;
 import org.springframework.context.ApplicationEvent;
 
 @Getter
-public class CreateLogEntryEvent extends ApplicationEvent implements AblyPublishableEvent {
+public class CreateLogEntryEvent extends ApplicationEvent {
     private final CreateLogEntryRequest createLogEntryRequest;
 
     public CreateLogEntryEvent(Object source, CreateLogEntryRequest createLogEntryRequest) {
         super(source);
         this.createLogEntryRequest = createLogEntryRequest;
-    }
-
-    @Override
-    public String getChannelName() {
-        return LogEntryChannelNames.adminLogs();
-    }
-
-    @Override
-    public String getEventName() {
-        return "admin.log.created";
     }
 }
 
--- src/main/java/org/phong/horizon/admin/logentry/services/LogService.java
+++ src/main/java/org/phong/horizon/admin/logentry/services/LogService.java
@@ -1,6 +1,7 @@
 package org.phong.horizon.admin.logentry.services;
 
 import lombok.AllArgsConstructor;
+import org.phong.horizon.ably.event.AblyPublishableEvent;
 import org.phong.horizon.admin.logentry.dtos.CreateLogEntryRequest;
 import org.phong.horizon.admin.logentry.dtos.LogEntryDto;
 import org.phong.horizon.admin.logentry.dtos.LogSearchCriteriaDto;
@@ -10,8 +11,10 @@ import org.phong.horizon.admin.logentry.infrastructure.entities.LogEntry;
 import org.phong.horizon.admin.logentry.infrastructure.mapstruct.LogMapper;
 import org.phong.horizon.admin.logentry.infrastructure.repositories.LogEntryRepository;
 import org.phong.horizon.admin.logentry.infrastructure.specifications.LogSpecification;
+import org.phong.horizon.admin.logentry.utils.LogEntryChannelNames;
 import org.phong.horizon.user.infrastructure.persistence.entities.User;
 import org.phong.horizon.user.infrastructure.persistence.repositories.UserRepository;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -19,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.OffsetDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -29,6 +33,7 @@ public class LogService {
     private final UserRepository userRepository;
     private final LogMapper logMapper;
     private final LogSpecification logSpecification;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public LogEntryDto createLogEntry(CreateLogEntryRequest request) {
@@ -42,7 +47,26 @@ public class LogService {
         }
 
         LogEntry savedLogEntry = logEntryRepository.save(logEntry);
-        return logMapper.toDto(savedLogEntry);
+        LogEntryDto mapperDto = logMapper.toDto(savedLogEntry);
+        eventPublisher.publishEvent(new AblyPublishableEvent() {
+            @Override
+            public String getChannelName() {
+                return LogEntryChannelNames.adminLogs();
+            }
+
+            @Override
+            public String getEventName() {
+                return "admin.log.created";
+            }
+
+            @Override
+            public Map<String, Object> getPayload() {
+                return Map.of(
+                        "logEntry", mapperDto
+                );
+            }
+        });
+        return mapperDto;
     }
 
     public Page<LogEntryDto> getAllLogEntries(Pageable pageable, LogSearchCriteriaDto criteria) {
